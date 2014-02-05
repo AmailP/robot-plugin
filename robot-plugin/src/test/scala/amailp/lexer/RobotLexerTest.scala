a07c2c3fc2e5dcdd5e04549a3bb71473efd3ec23
Fabio Lombardelli <fabio.lombardelli@here.com>
2014-02-05 18:05:53 +0100

    Converted RobotLexerTest from Java to Scala

---
+++ robot-plugin/src/test/scala/amailp/lexer/RobotLexerTest.scala
@@ -0,0 +1,98 @@
+package amailp.lexer
+
+import java.util.Scanner;
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import com.intellij.psi.tree.IElementType
+
+import amailp.elements.RobotTokenTypes
+
+class _BaseLexerTest extends FunSuite with BeforeAndAfter {
+  var robotLexer: RobotLexer = null
+
+  before {
+    robotLexer = new RobotLexer
+  }
+
+  after {
+    def checkAllWasLexed() = assert(robotLexer.getTokenStart == robotLexer.getTokenEnd, "Something was left unlexed")
+    checkAllWasLexed()
+  }
+
+  def scanString(toBeScanned: String) {
+    robotLexer.start(toBeScanned)
+  }
+
+  def nextTokenIsType(elementType: IElementType) {
+    assert(elementType == robotLexer.getTokenType)
+    robotLexer.advance()
+  }
+
+  def nextTokenIs(text: String, elementType: IElementType) {
+    assert(text == robotLexer.getTokenText)
+    nextTokenIsType(elementType)
+  }
+}
+
+class RobotLexerTest extends _BaseLexerTest {
+  test("Variable") {
+    scanString("${a_Variable}")
+    nextTokenIsType(RobotTokenTypes.Variable)
+  }
+
+  test("ListVariable") {
+    scanString("@{aListVariable}")
+    nextTokenIsType(RobotTokenTypes.ListVariable)
+  }
+
+  test("Word") {
+    scanString("ThisIsAWord")
+    nextTokenIsType(RobotTokenTypes.Word)
+  }
+
+  test("WordWithSymbols") {
+    scanString("!IsThisAWord??.")
+    nextTokenIsType(RobotTokenTypes.Word)
+  }
+
+  test("testCell") {
+    scanString("    A cell")
+    nextTokenIsType(RobotTokenTypes.Separator)
+    nextTokenIs("A", RobotTokenTypes.Word)
+    nextTokenIsType(RobotTokenTypes.Space)
+    nextTokenIs("cell", RobotTokenTypes.Word)
+  }
+
+  test("VariableCell") {
+    scanString("  This ${is} cell  \n")
+    nextTokenIsType(RobotTokenTypes.Separator)
+    nextTokenIs("This", RobotTokenTypes.Word)
+    nextTokenIsType(RobotTokenTypes.Space)
+    nextTokenIs("${is}", RobotTokenTypes.Variable)
+    nextTokenIsType(RobotTokenTypes.Space)
+    nextTokenIs("cell", RobotTokenTypes.Word)
+    nextTokenIsType(RobotTokenTypes.IrrelevantSpaces)
+    nextTokenIsType(RobotTokenTypes.LineTerminator)
+  }
+
+  test("Comment") {
+    scanString("  #This is comment  ")
+    nextTokenIsType(RobotTokenTypes.IrrelevantSpaces)
+    nextTokenIs("#This is comment  ", RobotTokenTypes.Comment)
+  }
+
+  test("BlankLine") {
+    scanString("   \n")
+    nextTokenIsType(RobotTokenTypes.BlankLine)
+  }
+
+  test("All") {
+    val s: String = new Scanner(this.getClass.getClassLoader.getResourceAsStream("complete.robot")).useDelimiter("\\A").next
+    scanString(s)
+    while (robotLexer.getTokenType != null) {
+      System.out.println(robotLexer.getTokenType + "\t\t" + robotLexer.getTokenText)
+      robotLexer.advance
+    }
+  }
+}
