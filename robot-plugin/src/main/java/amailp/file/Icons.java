4486f9ffe558be752ab61b2d13c61fefc551a5f8
Valerio Angelini <amailp@gmail.com>
2014-02-17 08:19:21 +0100

    New icons added

--- robot-plugin/src/main/java/amailp/file/Icons.java
+++ robot-plugin/src/main/java/amailp/file/Icons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public class Icons {
-    public static final Icon FILE = IconLoader.getIcon("/amailp/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/robo_file_16.png");
 }
