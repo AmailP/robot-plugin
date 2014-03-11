2584b2859e2a71a56cf267ac311c52cf79b33890
Valerio Angelini <amailp@gmail.com>
2014-03-11 19:59:56 +0100

    jump to keywords takes in consideration ignorable prefixes

--- src/main/scala/amailp/intellij/robot/psi/Keyword.scala
+++ src/main/scala/amailp/intellij/robot/psi/Keyword.scala
@@ -1,7 +1,7 @@
 package amailp.intellij.robot.psi
 
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiReference, AbstractElementManipulator}
+import com.intellij.psi.{PsiElement, PsiReference, AbstractElementManipulator}
 import com.intellij.openapi.util.TextRange
 import scala.collection.JavaConversions._
 import com.intellij.lang.ASTNode
@@ -9,23 +9,51 @@ import com.intellij.extapi.psi.ASTWrapperPsiElement
 
 case class Keyword(node: ASTNode) extends ASTWrapperPsiElement(node) {
   override lazy val getReference: PsiReference = new KeywordReference(this)
+
+  lazy val getTextStrippedFromIgnored = (for {
+    prefix <- List("given", "when", "then", "and")
+    if getText.toLowerCase.startsWith(prefix)
+    stripped = getText.toLowerCase.replaceFirst(prefix, "").trim
+  } yield stripped).headOption
 }
 
 class KeywordReference(element: Keyword) extends RobotReferenceBase[Keyword](element) {
 
   def sameTextAsKeyword(keywordName: KeywordName) = keywordName.getText equalsIgnoreCase element.getText
 
+  private def findKeywordNamesInRobotFiles(files: Iterable[RobotPsiFile], original: String = element.getText) = {
+    for {
+      file <- files
+      fileKeywordName <- file.getDefinedKeywordNames
+      if fileKeywordName.getText equalsIgnoreCase original
+    } yield fileKeywordName
+  }
+
   override def resolve() = {
-    fileDefinedKeywordNames find sameTextAsKeyword match {
-      case Some(keyword) => keyword.getParent
-      case None => currentRobotFile.getRecursivelyImportedRobotFiles
-          .flatMap(_.getDefinedKeywordNames)
-          .find(sameTextAsKeyword) match {
-          case Some(keyword) => keyword.getParent
+    findKeywordNamesInRobotFiles(List(currentRobotFile)).headOption match {
+      case Some(keyword) => keyword
+      case None => findKeywordNamesInRobotFiles(currentRobotFile.getRecursivelyImportedRobotFiles).headOption match {
+        case Some(keyword) => keyword
+        case None => element.getTextStrippedFromIgnored match {
+          case Some(strippedKeywordName) => findKeywordNamesInRobotFiles(List(currentRobotFile), strippedKeywordName).headOption match {
+            case Some(keyword) => keyword
+            case None => findKeywordNamesInRobotFiles(currentRobotFile.getRecursivelyImportedRobotFiles, strippedKeywordName).headOption match {
+              case Some(keyword) => keyword
+              case None => null
+            }
+          }
           case None => null
         }
+      }
     }
   }
+
+  private def stripIgnored = for {
+    prefix <- List("given", "when", "then", "and")
+    if element.getText.toLowerCase.startsWith(prefix)
+    stripped = element.getText.toLowerCase.replaceFirst(prefix, "").trim
+  } yield stripped
+
 
   override def getVariants: Array[AnyRef] = {
     val externalKeywordNames: Set[KeywordName] = for {
@@ -34,12 +62,12 @@ class KeywordReference(element: Keyword) extends RobotReferenceBase[Keyword](ele
     } yield externalKeywordName
 
     for {
-      keywordname <- (externalKeywordNames | fileDefinedKeywordNames.toSet).toArray
-    } yield keywordname.getText
+      keywordName <- (externalKeywordNames | fileDefinedKeywordNames.toSet).toArray
+    } yield keywordName.getText
   }
 
   private def fileDefinedKeywordNames = {
-    PsiTreeUtil findChildrenOfType(currentRobotFile, classOf[KeywordName])
+    findKeywordNamesInRobotFiles(List(currentRobotFile))
   }
 }
 
