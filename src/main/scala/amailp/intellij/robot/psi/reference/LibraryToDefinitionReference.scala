fef63939f67fa92caa80c306891372d7f7361071
Iurii Kudriavtsev <iurii.kudriavtsev@here.com>
2016-01-11 18:23:27 +0100

    Now on-click navigates to dotted path.

--- src/main/scala/amailp/intellij/robot/psi/reference/LibraryToDefinitionReference.scala
+++ src/main/scala/amailp/intellij/robot/psi/reference/LibraryToDefinitionReference.scala
@@ -4,8 +4,11 @@ import amailp.intellij.robot.psi.utils.ExtRobotPsiUtils
 import amailp.intellij.robot.psi.LibraryValue
 import com.intellij.codeInsight.lookup.{AutoCompletionPolicy, LookupElementBuilder}
 import com.intellij.psi.{PsiElement, PsiElementResolveResult, ResolveResult, PsiPolyVariantReferenceBase}
+import com.intellij.psi.util.QualifiedName
+import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.stubs.{PyClassNameIndex, PyModuleNameIndex}
 import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 
 class LibraryToDefinitionReference(library: LibraryValue) extends PsiPolyVariantReferenceBase[LibraryValue](library)
@@ -22,9 +25,18 @@ class LibraryToDefinitionReference(library: LibraryValue) extends PsiPolyVariant
   }.toArray
 
   override def multiResolve(incompleteCode: Boolean): Array[ResolveResult] = {
+    val qName = QualifiedName.fromDottedString(library.getText)
+    var modules = mutable.MutableList[PyFile]()
+    if (qName.getComponentCount > 1) {
+      for {
+        psiFile <- PyModuleNameIndex.find(qName.getLastComponent, utilsPsiElement.getProject, true)
+        if psiFile.getPresentation.getLocationString contains qName.getComponents.dropRight(1).mkString(".")
+      } modules += psiFile
+    } else {
+      modules ++= PyModuleNameIndex.find(library.getText, utilsPsiElement.getProject, true)
+    }
     for {
-      psiFile <- PyModuleNameIndex.find(library.getText, utilsPsiElement.getProject, true) ++
-        PyClassNameIndex.find(library.getText, utilsPsiElement.getProject, true) ++
+      psiFile <- modules ++ PyClassNameIndex.find(library.getText, utilsPsiElement.getProject, true) ++
         Option(PyClassNameIndex.findClass(library.getText, utilsPsiElement.getProject))
     } yield new PsiElementResolveResult(psiFile)
   }.toArray
