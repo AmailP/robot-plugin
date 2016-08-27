3b4492687c2940e87ce12a5f87491382ca341458
Valerio Angelini <amailp@gmail.com>
2016-08-27 22:08:58 +0200

    Fix exception being thrown when control click on unreferenced library values

--- src/main/scala/amailp/intellij/robot/psi/LibraryValue.scala
+++ src/main/scala/amailp/intellij/robot/psi/LibraryValue.scala
@@ -3,24 +3,15 @@ package amailp.intellij.robot.psi
 import amailp.intellij.robot.psi.utils.RobotPsiUtils
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiReference
 import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
-import com.intellij.psi.{PsiReference, PsiElement, PsiNameIdentifierOwner}
 
 
-class LibraryValue(node: ASTNode) extends ASTWrapperPsiElement(node) with RobotPsiUtils with Library with PsiNameIdentifierOwner {
-
-  override def getName: String = getText
-
-  override def setName(name: String): PsiElement = {
-//    No refactoring support yet, thus skipping.
-    this
-  }
+class LibraryValue(node: ASTNode) extends ASTWrapperPsiElement(node) with RobotPsiUtils with Library {
 
   def getType: String = "Library"
 
   override def getText: String = getNode.getText
-
-  override def getNameIdentifier: PsiElement = this
 
   override def getReferences: Array[PsiReference] = ReferenceProvidersRegistry.getReferencesFromProviders(this)
 }
