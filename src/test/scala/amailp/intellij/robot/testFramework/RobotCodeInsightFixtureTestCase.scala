1351f73274f2c8f33e8fefd1cbfe909522fc65d1
Valerio Angelini <valerio.angelini@here.com>
2018-11-09 22:17:20 +0100

    Use updated version of gradle intellij plugin

--- src/test/scala/amailp/intellij/robot/testFramework/RobotCodeInsightFixtureTestCase.scala
+++ src/test/scala/amailp/intellij/robot/testFramework/RobotCodeInsightFixtureTestCase.scala
@@ -4,9 +4,11 @@ import java.io.File
 
 import com.intellij.psi.{PsiPolyVariantReference, ResolveResult}
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import org.junit.Ignore
 import org.scalatest.Matchers
 import org.scalatest.junit.AssertionsForJUnit
 
+@Ignore
 class RobotCodeInsightFixtureTestCase extends LightPlatformCodeInsightFixtureTestCase with Matchers with AssertionsForJUnit  {
   override def getTestDataPath =
     new File(this.getClass.getClassLoader.getResource("complete.robot").toURI).getParent
