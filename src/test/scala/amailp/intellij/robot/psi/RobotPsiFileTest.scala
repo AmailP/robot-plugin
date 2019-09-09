895f100b19eed137398536bb0561f59983de3415
Sergey Khomutinin <skhomuti@gmail.com>
2019-09-09 22:20:10 +0500

    Support for ${/} path separator for resource files
    Update src/main/scala/amailp/intellij/robot/psi/reference/ResourceValueReference.scala
    
    Co-Authored-By: Valerio Angelini <amailp@gmail.com>

--- src/test/scala/amailp/intellij/robot/psi/RobotPsiFileTest.scala
+++ src/test/scala/amailp/intellij/robot/psi/RobotPsiFileTest.scala
@@ -23,6 +23,20 @@ class RobotPsiFileTest extends RobotCodeInsightFixtureTestCase {
     imported should contain (someKeywords)
   }
 
+  def testImportedRobotFilesWithPathSeparator(): Unit = {
+    val files = myFixture.configureByFiles(
+      "RobotPsiFileTest/include_some_keywords_with_path_separator.robot",
+      "RobotPsiFileTest/some_keywords.robot"
+    ).map(_.asInstanceOf[RobotPsiFile])
+
+    val includeSomeKeywords = files.head
+    val someKeywords = files(1)
+
+    val imported = includeSomeKeywords.getRecursivelyImportedRobotFiles
+    imported should have size 1
+    imported should contain (someKeywords)
+  }
+
   def testRecursivelyImportedRobotFiles(): Unit = {
     val files = myFixture.configureByFiles(
       "RobotPsiFileTest/include_some_other_keywords.robot",
