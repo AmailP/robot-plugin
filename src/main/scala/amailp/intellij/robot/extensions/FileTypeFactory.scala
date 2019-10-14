3b4fd0332467f9748769f66a1fcc3fb3d3c8a57b
Valerio Angelini <valerio.angelini@here.com>
2019-10-14 06:34:34 +0200

    Remove usage of deprecated fileTypeFactory extension

--- src/main/scala/amailp/intellij/robot/extensions/FileTypeFactory.scala
+++
@@ -1,8 +0,0 @@
-package amailp.intellij.robot.extensions
-
-import com.intellij.openapi.fileTypes.FileTypeConsumer
-import amailp.intellij.robot.file.FileType
-
-class FileTypeFactory extends com.intellij.openapi.fileTypes.FileTypeFactory {
-  def createFileTypes(consumer: FileTypeConsumer) = consumer.consume(FileType, "robot")
-}
