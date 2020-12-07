ad1cd82489e9afed9488330b0a03f0b4bce7a500
Valerio Angelini <862998+AmailP@users.noreply.github.com>
2020-12-07 09:15:22 +0100

    Replaced deprecated method usage

--- src/main/scala/amailp/intellij/robot/file/Icons.scala
+++ src/main/scala/amailp/intellij/robot/file/Icons.scala
@@ -1,15 +1,15 @@
 package amailp.intellij.robot.file
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 import com.intellij.ui.LayeredIcon
-import com.intellij.icons.AllIcons
 
 object Icons {
-  val file = IconLoader.getIcon("/icons/robot_file_16.png")
-  val robot = IconLoader.getIcon("/icons/robot_16.png")
+  val file = IconLoader.getIcon("/icons/robot_file_16.png", Icons.getClass)
+  val robot = IconLoader.getIcon("/icons/robot_16.png", Icons.getClass)
   val robotTest = new LayeredIcon(robot, AllIcons.Nodes.JunitTestMark)
-  val keywords = IconLoader.getIcon("/icons/keywords_16.png")
-  val keyword = IconLoader.getIcon("/icons/keyword_16.png")
-  val variables = IconLoader.getIcon("/icons/variables_16.png")
-  val variable = IconLoader.getIcon("/icons/variable_16.png")
+  val keywords = IconLoader.getIcon("/icons/keywords_16.png", Icons.getClass)
+  val keyword = IconLoader.getIcon("/icons/keyword_16.png", Icons.getClass)
+  val variables = IconLoader.getIcon("/icons/variables_16.png", Icons.getClass)
+  val variable = IconLoader.getIcon("/icons/variable_16.png", Icons.getClass)
 }
